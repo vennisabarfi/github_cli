aab22f3663fee5288be4f4b360284baaedecb584
veyhn <vennisabarfi@gmail.com>
2024-06-25 19:16:35 -0400

    Prompts expanded

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 const express = require('express');
 const app = express();
-const axios = require('axios').default;
-const yargs = require('yargs');
 const PORT = 3000;
-let argv = yargs.argv; //parse command line arguments 
 
-
-// Use yarg
 app.listen(PORT, function(){
     try {
         console.log(`Server is running on Port ${PORT}`);
--- cli.js
+++ cli.js
@@ -1,11 +1,17 @@
 const {Command} = require("commander");
 const program  = new Command();
 const chalk = require("chalk");
-const inquirer = require('inquirer'); //read documentation  
+const inquirer = require('inquirer'); //read documentation 
 const ora = require("ora");
 const {octokit, Octokit} = require("octokit"); 
-require('dotenv').config(); //load .env file
-console.log(process.env.github_TOKEN);
+
+try {
+    require('dotenv').config(); //load .env file
+} catch (error) {
+    console.log(`Error loading secret keys. Info: ${error}`);
+}
+
+// console.log(process.env.github_TOKEN);
 
 
 const ocktokit = new Octokit({
@@ -15,17 +21,34 @@ const ocktokit = new Octokit({
 //or no authentication(lower rate limit and limited endpoints)
 //const octokit = new Octokit({});
 
+//add try and catch statements to authenticate information given for owner, repo and ref
 program.version("1.0.0").description("My Node CLI");
 
  program.action(function(){
     inquirer
     .prompt([
         {
+            name: "owner",
+            type: "input",
+            message: "Enter your github username: "
+        },
+        {
+            name: "repo",
+            type: "input",
+            message: "Enter the github repo name: "
+        },
+        {
+            name: "ref",
+            type: "input",
+            message: "Enter the branch name: "
+        },
+        {
             type: "list",
             name: "choice",
             message: "Choose an option: ",
-            choices: ["Option 1", "Option 2","Option 3"],
-        }
+            choices: ["Track Commit Status: ", "Option 2: ","Option 3: "],
+        },
+     
     ])
     .then(function(result){
         const spinner = ora(`Doing ${result.choice}...`).start(); //Start the spinner
